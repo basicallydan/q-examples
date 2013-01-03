ac6eb70733fe701f272df5e9bcb0696061b03650
unknown <daniel.hough@gmail.com>
2013-01-03 22:42:35 +0000

    Added an example with concurrent promises

--- wait-for-multiple-promises.js
+++ wait-for-multiple-promises.js
@@ -2,27 +2,30 @@ var Q = require("q");
 
 var oneA = function () {
     var d = Q.defer();
+	var timeUntilResolve = Math.floor((Math.random()*2000)+1);
     console.log('1A Starting');
     setTimeout(function () {
 		console.log('1A Finished');
-		d.resolve();
-    }, Math.floor((Math.random()*5000)+1));
+		d.resolve('1ATime: ' + timeUntilResolve);
+    }, timeUntilResolve);
     return d.promise;
 };
 
 var oneB = function () {
 	var d = Q.defer();
+	var timeUntilResolve = Math.floor((Math.random()*2000)+1);
 	console.log('1B Starting');
 	setTimeout(function () {
 		console.log('1B Finished');
-		d.resolve();
-    }, Math.floor((Math.random()*2000)+1));
+		d.resolve('1BTime: ' + timeUntilResolve);
+    }, timeUntilResolve);
 	return d.promise;
 };
 
 // This fuction throws an error which later on we show will be handled
-var two = function () {
+var two = function (oneATime, oneBTime) {
 	var d = Q.defer();
+	console('OneA: ' + oneATime + ', OneB: ' + oneBTime);
 	console.log('2 Starting and Finishing, so 3A and 3B should start');
 	d.resolve();
 	return d.promise;
@@ -53,7 +56,7 @@ var four = function () {
 };
 
 Q.allResolved([ oneA(), oneB() ])
-.then(two)
+.spread(two)
 .then(function () { return Q.all([ threeA(), threeB() ]); })
 .then(four)
 .done();
